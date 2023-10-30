081c6f0a898925c69093421c29cb0610a2038125
Tomás Pollak <tomaspollak@gmail.com>
2023-10-30 17:36:29 -0300

    Fix action button state init (from PR #144)

--- src/index.js
+++ src/index.js
@@ -354,7 +354,7 @@ export default class ImageTool {
     this._data.caption = data.caption || '';
     this.ui.fillCaption(this._data.caption);
 
-    ImageTool.tunes.forEach(({ name: tune }) => {
+    ImageTool.tunes.concat(this.config.actions || []).forEach(({ name: tune }) => {
       const value = typeof data[tune] !== 'undefined' ? data[tune] === true || data[tune] === 'true' : false;
 
       this.setTune(tune, value);
